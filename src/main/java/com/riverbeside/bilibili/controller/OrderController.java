3ca04a5a548a16a1184e0be63f911e488b1b1288
linny <linz@jvtd.cn>
2023-06-13 17:09:27 +0800

    litle

--- src/main/java/com/riverbeside/bilibili/controller/OrderController.java
+++ src/main/java/com/riverbeside/bilibili/controller/OrderController.java
@@ -4,6 +4,7 @@ package com.riverbeside.bilibili.controller;
 import com.riverbeside.bilibili.entity.Order;
 import com.riverbeside.bilibili.service.IOrderService;
 import com.riverbeside.bilibili.util.JsonResult;
+import com.riverbeside.bilibili.util.ThreadService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +17,14 @@ public class OrderController extends BaseController{
     @Autowired
     IOrderService orderService;
 
-
+    @Autowired
+    ThreadService threadService;
     @RequestMapping("createOrder")
     public JsonResult<Order> createOrder(Integer aid, HttpSession session, Integer[] cids){
         Integer uid = getUidFromSession(session);
         String username = getUsernameFromSession(session);
         Order order = orderService.createOrder(uid,aid,username,cids);
-
+        threadService.minCount(uid,aid,username,cids);
         return new JsonResult<>(OK,order);
     }
 
